a7819223d407fa8ac886c34e2078878972a415be
cwroblewski <wroblewskic@o2.pl>
2022-01-25 21:22:25 +0100

    ...

--- proba_strzelnicza/views.py
+++ proba_strzelnicza/views.py
@@ -13,8 +13,4 @@ def shot(request, sample_id):
     shot = Shot.objects.get(sample_id=sample_id)
     return render(request, "proba_strzelnicza.html", {
         "proba_strzelnicza": shot
-
-
-
-
     })
